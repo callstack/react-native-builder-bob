eded5c2de6cb8cbbbfffb9f6adca2f3a1d8ea706
satyajit.happy <satyajit.happy@gmail.com>
2019-04-14 11:01:29 +0200

    fix: improve UX for building typescript

--- src/targets/typescript.ts
+++ src/targets/typescript.ts
@@ -1,6 +1,7 @@
 import chalk from 'chalk';
 import path from 'path';
 import child_process from 'child_process';
+import fs from 'fs-extra';
 import del from 'del';
 import { Input } from '../types';
 
@@ -11,14 +12,40 @@ export default async function build({ root, output, report }: Input) {
 
   report.info(`Generating type definitions with ${chalk.blue('tsc')}`);
 
-  child_process.execFileSync(path.join(root, 'node_modules', '.bin', 'tsc'), [
-    '--declaration',
-    '--emitDeclarationOnly',
-    '--outDir',
-    output,
-  ]);
+  const tsc = path.join(root, 'node_modules', '.bin', 'tsc');
 
-  report.success(
-    `Wrote definition files to ${chalk.blue(path.relative(root, output))}`
-  );
+  try {
+    if (await fs.pathExists(tsc)) {
+      child_process.execFileSync(tsc, [
+        '--declaration',
+        '--emitDeclarationOnly',
+        '--outDir',
+        output,
+      ]);
+
+      report.success(
+        `Wrote definition files to ${chalk.blue(path.relative(root, output))}`
+      );
+    } else {
+      throw new Error(
+        `The ${chalk.blue(
+          'tsc'
+        )} binary doesn't seem to be installed under ${chalk.blue(
+          'node_modules'
+        )}. Make sure you have added ${chalk.blue(
+          'typescript'
+        )} to your ${chalk.blue('devDependencies')}.`
+      );
+    }
+  } catch (e) {
+    if (e.stdout) {
+      report.error(
+        `Errors found when building definition files.\n${e.stdout.toString()}`
+      );
+    } else {
+      report.error(e.message);
+    }
+
+    throw new Error('Failed to build definition files.');
+  }
 }
