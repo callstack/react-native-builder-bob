5fb0d2de04f069ce533736ac30a4b80e3e4135b7
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-03-05 22:06:04 +0100

    fix: throw if no tsconfig.json found
    closes #17

--- src/targets/typescript.ts
+++ src/targets/typescript.ts
@@ -72,6 +72,10 @@ export default async function build({
           `Couldn't parse '${project}'. There might be validation errors.`
         );
       }
+    } else {
+      throw new Error(
+        `Couldn't find a ${chalk.blue('tsc')} in the project root.`
+      );
     }
 
     let tsc =
