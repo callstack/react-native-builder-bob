c383a63580320f61ad9c2bfb3d6004513f9a9547
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-05-09 21:34:19 +0200

    refactor: update circleci config

--- templates/cpp-library/ios/<%= project.name %>.mm
+++ templates/cpp-library/ios/<%= project.name %>.mm
@@ -5,6 +5,7 @@
 RCT_EXPORT_MODULE()
 
 // Example method for C++
+// See the implementation of the example module in the `cpp` folder
 RCT_EXPORT_METHOD(multiply:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
                   withResolver:(RCTPromiseResolveBlock)resolve
                   withReject:(RCTPromiseRejectBlock)reject)
