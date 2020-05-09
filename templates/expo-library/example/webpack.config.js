41301bdcf3956b50762c0fea6f3c05672668f320
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-05-09 21:14:24 +0200

    refactor: use babel-plugin-module-resolver for aliasing

--- templates/expo-library/example/webpack.config.js
+++ templates/expo-library/example/webpack.config.js
@@ -1,25 +1,24 @@
 const path = require('path');
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const { resolver } = require('./metro.config');
 
 const root = path.resolve(__dirname, '..');
-const node_modules = path.resolve(__dirname, 'node_modules');
+const node_modules = path.join(__dirname, 'node_modules');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
   config.module.rules.push({
     test: /\.(js|ts|tsx)$/,
-    include: path.resolve('../src'),
+    include: path.resolve(root, 'src'),
     use: 'babel-loader',
   });
 
+  // We need to make sure that only one version is loaded for peerDependencies
+  // So we alias them to the versions in example's node_modules
   Object.assign(config.resolve.alias, {
-    'react': path.join(node_modules, 'react'),
+    ...resolver.extraNodeModules,
     'react-native-web': path.join(node_modules, 'react-native-web'),
-    '<%= project.slug %>': path.join(
-      root,
-      require('../package.json').source
-    ),
   });
 
   return config;
