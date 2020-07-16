d74d26ed4e7ffca486dcfb7cd85d9b604ef1068c
Krzysztof Borowy <krizzu.dev@gmail.com>
2020-07-16 09:56:55 +0200

    fix: separate example from native lib

--- templates/example/example/android/app/src/main/java/com/example/{%= project.package %}/MainApplication.java
+++ templates/example/example/android/app/src/main/java/com/example/{%= project.package %}/MainApplication.java
@@ -10,8 +10,9 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
+<% if (project.module) { -%>
 import com.<%= project.package %>.<%= project.name %>Package;
+<% } -%>
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for <%= project.name %>Example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new <%= project.name %>Package());
+          <% if (project.module) { -%>packages.add(new <%= project.name %>Package());<% } -%>
 
           return packages;
         }
