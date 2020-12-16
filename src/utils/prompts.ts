2a5d0fd2e6b222477db43689aaa8dd3d4ae3d953
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-12-16 04:37:33 +0100

    refactor: replace inquirer with prompts
    prompts is much smaller than inquirer, so this should help in installing the CLI faster.

---
+++ src/utils/prompts.ts
@@ -0,0 +1,17 @@
+import promptsModule from 'prompts';
+
+export default function prompts(
+  args: promptsModule.PromptObject | promptsModule.PromptObject[],
+  options?: promptsModule.Options
+) {
+  return promptsModule(args, {
+    onCancel() {
+      process.exit(1);
+    },
+    ...options,
+  });
+}
+
+export type PromptObject<
+  T extends string = string
+> = promptsModule.PromptObject<T>;
