e7a3f32b74c63dac1a3adf1a5663c4460578a149
Satyajit Sahoo <satyajit.happy@gmail.com>
2020-12-16 02:49:38 +0100

    chore: override yarn command to make repo setup easier
    Currently we need to run 'yarn bootstrap' to setup the repo. However it's easy to forget. This PR overrides the Yarn command to run the 'bootstrap' script if no arguments are specified.

--- src/create.ts
+++ src/create.ts
@@ -366,7 +366,7 @@ export default async function create(argv: yargs.Arguments<any>) {
 
       {magenta {bold Get started} with the project}{gray :}
 
-        {gray $} yarn bootstrap
+        {gray $} yarn
       ${Object.entries(platforms)
         .map(
           ([script, { name, color }]) => chalk`
