e29da54e0484a2c0967fb349a07d60e3d5eafc05
Bartosz Klonowski <Bartosz.Klonowski@callstack.com>
2021-05-26 12:43:39 +0200

    Add C# native module project template

---
+++ packages/create-react-native-library/templates/windows-csharp-library/windows/{%- project.name %}/{%- project.name %}NativeModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.ReactNative.Managed;
+
+namespace <%- project.name %>
+{
+    [ReactModule("<%- project.name %>")]
+    internal sealed class <%- project.name %>NativeModule
+    {
+        [ReactInitializer]
+        public void Initialize( ReactContext reactContext )
+        {
+            this.reactContext = reactContext;
+        }
+
+
+        private ReactContext reactContext;
+    }
+}
