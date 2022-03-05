bbf409cbb39b60fdd4c88a36224fb70d7869764f
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-03-05 03:55:29 +0100

    `blacklist` -> `exclusionList` migration for examples metro.config.js (#168)
    * Update react-native version to 0.64.0
    
    The react-native version needs to be updated to 0.64.x so it will
    support the blacklist -> exclusionList migration.
    0.64.0 has been chosen as the initial stable version.
    
    NOTE: For an incoming Windows support the 0.64.0 is also the required
    version of react-native
    
    * Change blacklist to exclusionList in example's metro config
    
    * Change resolver field to blacklistRE
    
    It turns out that in the metro there's still a blacklist used for a
    resolver field. This makes the metro to be unable to find the new word.
    To fix that, the blacklistRE field name is brought back.

--- packages/create-react-native-library/templates/common/example/metro.config.js
+++ packages/create-react-native-library/templates/common/example/metro.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const blacklist = require('metro-config/src/defaults/blacklist');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 const pak = require('../package.json');
 
@@ -14,9 +14,9 @@ module.exports = {
   watchFolders: [root],
 
   // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
+  // So we block them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: blacklist(
+    blacklistRE: exclusionList(
       modules.map(
         (m) =>
           new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
