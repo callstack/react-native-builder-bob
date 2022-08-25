95107b7e823b978e31aac5212e578943cee1179d
Clément Taboulot <clement-taboulot@hotmail.fr>
2022-08-25 18:26:44 +0200

    feat(ios): enable function generation by xCode with CodeGen interface

--- packages/create-react-native-library/templates/objc-library/ios/{%- project.name %}.h
+++ packages/create-react-native-library/templates/objc-library/ios/{%- project.name %}.h
@@ -1,5 +1,13 @@
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RN<%- project.name -%>Spec.h"
+#else
 #import <React/RCTBridgeModule.h>
+#endif
 
+#ifdef RCT_NEW_ARCH_ENABLED
+@interface <%- project.name -%> : NSObject <Native<%- project.name -%>Spec>
+#else
 @interface <%- project.name -%> : NSObject <RCTBridgeModule>
+#endif
 
 @end
