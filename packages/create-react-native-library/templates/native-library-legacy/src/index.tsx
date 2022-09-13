2bc26b4e0e5f7370a09437d5053ffc7273de0144
Satyajit Sahoo <satyajit.happy@gmail.com>
2022-09-13 14:07:53 +0200

    chore: expo managed workflow -> expo go

--- packages/create-react-native-library/templates/native-library-legacy/src/index.tsx
+++ packages/create-react-native-library/templates/native-library-legacy/src/index.tsx
@@ -4,7 +4,7 @@ const LINKING_ERROR =
   `The package '<%- project.slug -%>' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
+  '- You are not using Expo Go\n';
 
 const <%- project.name -%> = NativeModules.<%- project.name -%>
   ? NativeModules.<%- project.name -%>
