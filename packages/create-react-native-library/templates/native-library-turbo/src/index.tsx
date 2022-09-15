f78495a1393920c22765c97b87522232c25f5b5e
Mateusz Wit <maateew@gmail.com>
2022-09-15 22:11:20 +0200

    feat: add missing scenarios

--- packages/create-react-native-library/templates/native-library-turbo/src/index.tsx
+++ packages/create-react-native-library/templates/native-library-turbo/src/index.tsx
@@ -1,4 +1,4 @@
-const <%- project.name -%> = require('./Native<%- project.name -%>').default
+const <%- project.name -%> = require('./Native<%- project.name -%>').default;
 
 export function multiply(a: number, b: number): number {
   return <%- project.name -%>.multiply(a, b);
