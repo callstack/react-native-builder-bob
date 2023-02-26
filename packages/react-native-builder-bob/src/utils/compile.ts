2eb6ed9f99223489e2fa56d860abc6e65fecd6b2
Satyajit Sahoo <satyajit.happy@gmail.com>
2023-02-26 19:48:16 +0100

    fix: use correct source file names relative to source dir for sourcemaps

--- packages/react-native-builder-bob/src/utils/compile.ts
+++ packages/react-native-builder-bob/src/utils/compile.ts
@@ -54,10 +54,12 @@ export default async function compile({
 
       const content = await fs.readFile(filepath, 'utf-8');
       const result = await babel.transformAsync(content, {
+        cwd: root,
         babelrc: babelrc,
         configFile: configFile,
         sourceMaps,
-        sourceRoot: path.relative(output, source),
+        sourceRoot: path.relative(path.dirname(outputFilename), source),
+        sourceFileName: path.relative(source, filepath),
         filename: filepath,
         ...(babelrc || configFile
           ? null
@@ -66,7 +68,6 @@ export default async function compile({
                 [
                   require.resolve('@babel/preset-env'),
                   {
-                    // @ts-ignore
                     targets: browserslist.findConfig(root) ?? {
                       browsers: [
                         '>1%',
