2c1c4e7cacd924ed8d0394c3644ccdc397a0efd5
Satyajit Sahoo <satyajit.happy@gmail.com>
2023-09-29 12:43:52 +0200

    fix: don't initialize git repo if we're already under a git repo

--- packages/create-react-native-library/src/index.ts
+++ packages/create-react-native-library/src/index.ts
@@ -775,15 +775,26 @@ async function create(argv: yargs.Arguments<any>) {
   }
 
   if (!local) {
+    let isInGitRepo = false;
+
     try {
-      await spawn('git', ['init'], { cwd: folder });
-      await spawn('git', ['branch', '-M', 'main'], { cwd: folder });
-      await spawn('git', ['add', '.'], { cwd: folder });
-      await spawn('git', ['commit', '-m', 'chore: initial commit'], {
-        cwd: folder,
-      });
+      isInGitRepo =
+        (await spawn('git', ['rev-parse', '--is-inside-work-tree'])) === 'true';
     } catch (e) {
       // Ignore error
+    }
+
+    if (!isInGitRepo) {
+      try {
+        await spawn('git', ['init'], { cwd: folder });
+        await spawn('git', ['branch', '-M', 'main'], { cwd: folder });
+        await spawn('git', ['add', '.'], { cwd: folder });
+        await spawn('git', ['commit', '-m', 'chore: initial commit'], {
+          cwd: folder,
+        });
+      } catch (e) {
+        // Ignore error
+      }
     }
   }
 
