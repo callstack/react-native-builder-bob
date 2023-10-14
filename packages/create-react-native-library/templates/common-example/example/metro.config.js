268a1df507bbe9652396c6f1d62188267d57bb3e
Burak Güner <burak.guner@callstack.com>
2023-10-14 11:49:15 +0300

    chore: change something

--- packages/create-react-native-library/templates/common-example/example/metro.config.js
+++ packages/create-react-native-library/templates/common-example/example/metro.config.js
@@ -4,6 +4,7 @@ const escape = require('escape-string-regexp');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 const pak = require('../package.json');
 
+// Hi mom!
 const root = path.resolve(__dirname, '..');
 const modules = Object.keys({ ...pak.peerDependencies });
 
