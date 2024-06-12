56e443f486210050ec36f91ae00552d9625afdb8
Burak Güner <burak.guner@callstack.com>
2024-06-12 14:35:59 +0300

    fix: prettier issue on view-module-legacy

--- packages/create-react-native-library/templates/native-view-library-legacy/src/index.tsx
+++ packages/create-react-native-library/templates/native-view-library-legacy/src/index.tsx
@@ -1,4 +1,4 @@
-import { <%- project.name -%> } from './<%- project.name -%>'
+import { <%- project.name -%> } from './<%- project.name -%>';
 
 export function multiply(a: number, b: number): Promise<number> {
   return <%- project.name -%>.multiply(a, b);
