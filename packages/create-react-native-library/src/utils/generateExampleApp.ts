64225716553181a84a3da129c01a572ef4e05a06
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-07-01 12:40:46 +0200

    feat: use metro for web bundling in js library template (#574)
    ### Summary
    
    This changes the bundler to Metro for the example app using Expo. It
    reduces the configuration required and aligns with the new defaults of
    Expo projects.
    
    ### Test plan
    
    - Create a library and choose the "JavaScript library" example
    - Ensure that the project runs on the web
    
    <img width="995" alt="image"
    src="https://github.com/callstack/react-native-builder-bob/assets/1174278/8dbe93dd-730f-49cd-9147-a7ec6f56e140">

--- packages/create-react-native-library/src/utils/generateExampleApp.ts
+++ packages/create-react-native-library/src/utils/generateExampleApp.ts
@@ -38,13 +38,9 @@ const PACKAGES_TO_ADD_DEV = {
 };
 
 const PACKAGES_TO_ADD_WEB = {
+  '@expo/metro-runtime': '~3.2.1',
   'react-dom': '18.2.0',
   'react-native-web': '~0.18.10',
-};
-
-const PACKAGES_TO_ADD_WEB_DEV = {
-  '@expo/webpack-config': '^18.0.1',
-  'babel-loader': '^8.1.0',
 };
 
 export default async function generateExampleApp({
@@ -159,10 +155,6 @@ export default async function generateExampleApp({
 
     Object.entries(PACKAGES_TO_ADD_WEB).forEach(([name, version]) => {
       dependencies[name] = bundledNativeModules[name] || version;
-    });
-
-    Object.entries(PACKAGES_TO_ADD_WEB_DEV).forEach(([name, version]) => {
-      devDependencies[name] = bundledNativeModules[name] || version;
     });
 
     scripts.web = 'expo start --web';
