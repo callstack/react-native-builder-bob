3a4e7240e9cabcdf76f45724485dbffa79daa011
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-07-26 14:12:07 +0200

    feat: use the bob preset for the library during dev (#599)
    Previously the React Native Babel preset was being used t compile the
    library code as well. However, this preset differs from the preset used
    to publish the library which can introduce inconsistencies.
    
    For example, Metro compiles modules in loose mode which can hide strict
    mode errors during dev, but they'll surface when consumer uses the
    library. With this change, we make sure that the same preset is used for
    dev to avoid the inconsistency.
    
    
    It also adds a babel plugin to explicitly add `use strict` - as Metro
    doesn't use strict mode for ESM code which is not spec-compliant, and
    the behavior differs from the CommonJS build where we add `use strict`
    during publish.

--- packages/create-react-native-library/templates/native-common-example/example/babel.config.js
+++ packages/create-react-native-library/templates/native-common-example/example/babel.config.js
@@ -1,17 +1,36 @@
 const path = require('path');
 const pak = require('../package.json');
 
+const root = path.resolve(__dirname, '..');
+
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        extensions: ['.tsx', '.ts', '.js', '.json'],
-        alias: {
-          [pak.name]: path.join(__dirname, '..', pak.source),
-        },
-      },
-    ],
+  overrides: [
+    {
+      exclude: path.join(root, 'src'),
+      presets: ['module:@react-native/babel-preset'],
+    },
+    {
+      include: path.join(root, 'src'),
+      presets: [
+        [
+          'module:react-native-builder-bob/babel-preset',
+          { modules: 'commonjs' },
+        ],
+      ],
+    },
+    {
+      exclude: /\/node_modules\//,
+      plugins: [
+        [
+          'module-resolver',
+          {
+            extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+            alias: {
+              [pak.name]: path.join(root, pak.source),
+            },
+          },
+        ],
+      ],
+    },
   ],
 };
