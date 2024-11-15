db6a4f43b8983f4def0cd141a46b6feb62ed336d
Burak Güner <burak.guner@callstack.com>
2024-11-16 02:05:05 +0300

    refactor: extract apply template block

--- packages/create-react-native-library/src/index.ts
+++ packages/create-react-native-library/src/index.ts
@@ -18,57 +18,9 @@ import { createInitialGitCommit } from './utils/initialCommit';
 import { assertAnswers, assertNpx } from './utils/assert';
 import { resolveBobVersionWithFallback } from './utils/promiseWithFallback';
 import { generateTemplateConfiguration } from './template/config';
-import { applyTemplate } from './template/applyTemplate';
+import { applyTemplates } from './template/applyTemplate';
 
 const FALLBACK_BOB_VERSION = '0.32.0';
-
-const COMMON_FILES = path.resolve(__dirname, '../templates/common');
-const COMMON_EXAMPLE_FILES = path.resolve(
-  __dirname,
-  '../templates/common-example'
-);
-const COMMON_LOCAL_FILES = path.resolve(__dirname, '../templates/common-local');
-const JS_FILES = path.resolve(__dirname, '../templates/js-library');
-const EXPO_FILES = path.resolve(__dirname, '../templates/expo-library');
-const CPP_FILES = path.resolve(__dirname, '../templates/cpp-library');
-const NATIVE_COMMON_FILES = path.resolve(
-  __dirname,
-  '../templates/native-common'
-);
-const NATIVE_COMMON_EXAMPLE_FILES = path.resolve(
-  __dirname,
-  '../templates/native-common-example'
-);
-
-const NATIVE_FILES = {
-  module_legacy: path.resolve(__dirname, '../templates/native-library-legacy'),
-  module_new: path.resolve(__dirname, '../templates/native-library-new'),
-  module_mixed: path.resolve(__dirname, '../templates/native-library-mixed'),
-  view_legacy: path.resolve(__dirname, '../templates/native-view-legacy'),
-  view_mixed: path.resolve(__dirname, '../templates/native-view-mixed'),
-  view_new: path.resolve(__dirname, '../templates/native-view-new'),
-} as const;
-
-const OBJC_FILES = {
-  module_common: path.resolve(__dirname, '../templates/objc-library'),
-  view_legacy: path.resolve(__dirname, '../templates/objc-view-legacy'),
-  view_mixed: path.resolve(__dirname, '../templates/objc-view-mixed'),
-  view_new: path.resolve(__dirname, '../templates/objc-view-new'),
-} as const;
-
-const KOTLIN_FILES = {
-  module_legacy: path.resolve(__dirname, '../templates/kotlin-library-legacy'),
-  module_new: path.resolve(__dirname, '../templates/kotlin-library-new'),
-  module_mixed: path.resolve(__dirname, '../templates/kotlin-library-mixed'),
-  view_legacy: path.resolve(__dirname, '../templates/kotlin-view-legacy'),
-  view_mixed: path.resolve(__dirname, '../templates/kotlin-view-mixed'),
-  view_new: path.resolve(__dirname, '../templates/kotlin-view-new'),
-} as const;
-
-const SWIFT_FILES = {
-  module_legacy: path.resolve(__dirname, '../templates/swift-library-legacy'),
-  view_legacy: path.resolve(__dirname, '../templates/swift-view-legacy'),
-} as const;
 
 type ArgName =
   | 'slug'
@@ -558,65 +510,7 @@ async function create(_argv: yargs.Arguments<any>) {
 
   spinner.text = 'Copying files';
 
-  if (local) {
-    await applyTemplate(config, COMMON_LOCAL_FILES, folder);
-  } else {
-    await applyTemplate(config, COMMON_FILES, folder);
-
-    if (config.example !== 'none') {
-      await applyTemplate(config, COMMON_EXAMPLE_FILES, folder);
-    }
-  }
-
-  if (answers.languages === 'js') {
-    await applyTemplate(config, JS_FILES, folder);
-    await applyTemplate(config, EXPO_FILES, folder);
-  } else {
-    await applyTemplate(config, NATIVE_COMMON_FILES, folder);
-
-    if (config.example !== 'none') {
-      await applyTemplate(config, NATIVE_COMMON_EXAMPLE_FILES, folder);
-    }
-
-    if (config.project.module) {
-      await applyTemplate(
-        config,
-        NATIVE_FILES[`module_${config.project.arch}`],
-        folder
-      );
-    } else {
-      await applyTemplate(
-        config,
-        NATIVE_FILES[`view_${config.project.arch}`],
-        folder
-      );
-    }
-
-    if (config.project.swift) {
-      await applyTemplate(config, SWIFT_FILES[`module_legacy`], folder);
-    } else {
-      if (config.project.module) {
-        await applyTemplate(config, OBJC_FILES[`module_common`], folder);
-      } else {
-        await applyTemplate(
-          config,
-          OBJC_FILES[`view_${config.project.arch}`],
-          folder
-        );
-      }
-    }
-
-    const templateType = `${config.project.module ? 'module' : 'view'}_${
-      config.project.arch
-    }` as const;
-
-    await applyTemplate(config, KOTLIN_FILES[templateType], folder);
-
-    if (config.project.cpp) {
-      await applyTemplate(config, CPP_FILES, folder);
-      await fs.remove(path.join(folder, 'ios', `${config.project.name}.m`));
-    }
-  }
+  await applyTemplates(answers, config, folder);
 
   const rootPackageJson = await fs.readJson(path.join(folder, 'package.json'));
 
--- packages/create-react-native-library/src/template/applyTemplate.ts
+++ packages/create-react-native-library/src/template/applyTemplate.ts
@@ -2,16 +2,144 @@ import path from 'path';
 import fs from 'fs-extra';
 import ejs from 'ejs';
 import type { TemplateConfiguration } from './config';
+import type { Answers } from 'create-react-native-library';
 
 const BINARIES = [
   /(gradlew|\.(jar|keystore|png|jpg|gif))$/,
   /\$\.yarn(?![a-z])/,
 ];
 
+const COMMON_FILES = path.resolve(__dirname, '../../templates/common');
+const COMMON_EXAMPLE_FILES = path.resolve(
+  __dirname,
+  '../../templates/common-example'
+);
+const COMMON_LOCAL_FILES = path.resolve(
+  __dirname,
+  '../../templates/common-local'
+);
+const JS_FILES = path.resolve(__dirname, '../../templates/js-library');
+const EXPO_FILES = path.resolve(__dirname, '../../templates/expo-library');
+const CPP_FILES = path.resolve(__dirname, '../../templates/cpp-library');
+const NATIVE_COMMON_FILES = path.resolve(
+  __dirname,
+  '../../templates/native-common'
+);
+const NATIVE_COMMON_EXAMPLE_FILES = path.resolve(
+  __dirname,
+  '../../templates/native-common-example'
+);
+
+const NATIVE_FILES = {
+  module_legacy: path.resolve(
+    __dirname,
+    '../../templates/native-library-legacy'
+  ),
+  module_new: path.resolve(__dirname, '../../templates/native-library-new'),
+  module_mixed: path.resolve(__dirname, '../../templates/native-library-mixed'),
+  view_legacy: path.resolve(__dirname, '../../templates/native-view-legacy'),
+  view_mixed: path.resolve(__dirname, '../../templates/native-view-mixed'),
+  view_new: path.resolve(__dirname, '../../templates/native-view-new'),
+} as const;
+
+const OBJC_FILES = {
+  module_common: path.resolve(__dirname, '../../templates/objc-library'),
+  view_legacy: path.resolve(__dirname, '../../templates/objc-view-legacy'),
+  view_mixed: path.resolve(__dirname, '../../templates/objc-view-mixed'),
+  view_new: path.resolve(__dirname, '../../templates/objc-view-new'),
+} as const;
+
+const KOTLIN_FILES = {
+  module_legacy: path.resolve(
+    __dirname,
+    '../../templates/kotlin-library-legacy'
+  ),
+  module_new: path.resolve(__dirname, '../../templates/kotlin-library-new'),
+  module_mixed: path.resolve(__dirname, '../../templates/kotlin-library-mixed'),
+  view_legacy: path.resolve(__dirname, '../../templates/kotlin-view-legacy'),
+  view_mixed: path.resolve(__dirname, '../../templates/kotlin-view-mixed'),
+  view_new: path.resolve(__dirname, '../../templates/kotlin-view-new'),
+} as const;
+
+const SWIFT_FILES = {
+  module_legacy: path.resolve(
+    __dirname,
+    '../../templates/swift-library-legacy'
+  ),
+  view_legacy: path.resolve(__dirname, '../../templates/swift-view-legacy'),
+} as const;
+
+export async function applyTemplates(
+  answers: Answers,
+  config: TemplateConfiguration,
+  folder: string
+) {
+  const { local } = answers;
+  if (local) {
+    await applyTemplate(config, COMMON_LOCAL_FILES, folder);
+  } else {
+    await applyTemplate(config, COMMON_FILES, folder);
+
+    if (config.example !== 'none') {
+      await applyTemplate(config, COMMON_EXAMPLE_FILES, folder);
+    }
+  }
+
+  if (answers.languages === 'js') {
+    await applyTemplate(config, JS_FILES, folder);
+    await applyTemplate(config, EXPO_FILES, folder);
+  } else {
+    await applyTemplate(config, NATIVE_COMMON_FILES, folder);
+
+    if (config.example !== 'none') {
+      await applyTemplate(config, NATIVE_COMMON_EXAMPLE_FILES, folder);
+    }
+
+    if (config.project.module) {
+      await applyTemplate(
+        config,
+        NATIVE_FILES[`module_${config.project.arch}`],
+        folder
+      );
+    } else {
+      await applyTemplate(
+        config,
+        NATIVE_FILES[`view_${config.project.arch}`],
+        folder
+      );
+    }
+
+    if (config.project.swift) {
+      await applyTemplate(config, SWIFT_FILES[`module_legacy`], folder);
+    } else {
+      if (config.project.module) {
+        await applyTemplate(config, OBJC_FILES[`module_common`], folder);
+      } else {
+        await applyTemplate(
+          config,
+          OBJC_FILES[`view_${config.project.arch}`],
+          folder
+        );
+      }
+    }
+
+    const templateType = `${config.project.module ? 'module' : 'view'}_${
+      config.project.arch
+    }` as const;
+
+    await applyTemplate(config, KOTLIN_FILES[templateType], folder);
+
+    if (config.project.cpp) {
+      await applyTemplate(config, CPP_FILES, folder);
+      await fs.remove(path.join(folder, 'ios', `${config.project.name}.m`));
+    }
+  }
+}
+
 /**
  * This copies the template files and renders them via ejs
  */
-export async function applyTemplate(
+async function applyTemplate(
   config: TemplateConfiguration,
   source: string,
   destination: string
