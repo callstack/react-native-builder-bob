a6d0f52ab71307be489506c11fa3b6f7159ced80
Burak Güner <burak.guner@callstack.com>
2024-11-30 00:07:49 +0300

    feat: add kotlin files

---
+++ packages/create-react-native-library/templates/nitro-module/android/src/main/java/com/margelo/nitro/{%- project.package_dir %}/{%- project.name %}Package.kt
@@ -0,0 +1,22 @@
+package com.margelo.nitro.<%- project.package %>
+
+import com.facebook.react.TurboReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.model.ReactModuleInfoProvider
+
+class CrnlNitroPackage : TurboReactPackage() {
+    override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
+        return null
+    }
+
+    override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
+        return ReactModuleInfoProvider { HashMap() }
+    }
+
+    companion object {
+        init {
+            System.loadLibrary("<%- project.name -%>")
+        }
+    }
+}
