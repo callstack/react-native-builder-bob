9fb5b5e5d274ee7b8c9253b1d2804be893a8d8a6
Burak Güner <burak.guner@callstack.com>
2024-11-29 23:43:13 +0300

    feat: add cpp adapter

---
+++ packages/create-react-native-library/templates/nitro-module/android/src/main/cpp/$cpp-adapter.cpp
@@ -0,0 +1,6 @@
+#include <jni.h>
+#include "<%- project.package_cpp -%>OnLoad.hpp"
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
+  return margelo::nitro::<%- project.package -%>::initialize(vm);
+}
