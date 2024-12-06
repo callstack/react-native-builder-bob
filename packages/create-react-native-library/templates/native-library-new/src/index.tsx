c999fb2d5083a4afa71fad49a0c7ca09241c9db6
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-12-06 17:58:53 +0100

    refactor: use import instead of require

--- packages/create-react-native-library/templates/native-library-new/src/index.tsx
+++ packages/create-react-native-library/templates/native-library-new/src/index.tsx
@@ -1,4 +1,4 @@
-const <%- project.name -%> = require('./Native<%- project.name -%>').default;
+import <%- project.name -%> from './Native<%- project.name -%>';
 
 export function multiply(a: number, b: number): number {
   return <%- project.name -%>.multiply(a, b);
