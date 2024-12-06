86c5c67656712a990f2dd1c9eda761c7082d52e6
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-12-06 19:00:13 +0100

    fix: fix codegen script failing due to missing platforms
    there is a regression in the codegen script that assumes `platforms` field to be present.
    so we add empty objects to avoid the crash during code generation.

--- packages/create-react-native-library/templates/native-common-example/example/react-native.config.js
+++ packages/create-react-native-library/templates/native-common-example/example/react-native.config.js
@@ -25,6 +25,12 @@ module.exports = {
   dependencies: {
     [pkg.name]: {
       root: path.join(__dirname, '..'),
+      platforms: {
+        // Codegen script incorrectly fails without this
+        // So we explicitly specify the platforms with empty object
+        ios: {},
+        android: {},
+      },
     },
   },
 };
