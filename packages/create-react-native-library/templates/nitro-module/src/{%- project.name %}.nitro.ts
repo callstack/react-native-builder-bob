1c89f260e95fcb883a6e5e9f0ec64f4e500b0a90
Burak Güner <burak.guner@callstack.com>
2024-12-17 15:50:40 +0300

    add newline in nitro ts

--- packages/create-react-native-library/templates/nitro-module/src/{%- project.name %}.nitro.ts
+++ packages/create-react-native-library/templates/nitro-module/src/{%- project.name %}.nitro.ts
@@ -1,6 +1,6 @@
 import type { HybridObject } from 'react-native-nitro-modules';
 
-export interface <%- project.name -%>
+export interface <%- project.name %>
   extends HybridObject<{ ios: 'swift'; android: 'kotlin' }> {
   multiply(a: number, b: number): number;
 }
