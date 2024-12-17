6084bf59dfa086bfd588d995b25fb3f3ad96e016
Burak Güner <burak.guner@callstack.com>
2024-12-17 17:42:59 +0300

    fix: use the correct cpp path

--- packages/create-react-native-library/templates/nitro-module/android/src/main/cpp/cpp-adapter.cpp
+++ packages/create-react-native-library/templates/nitro-module/android/src/main/cpp/cpp-adapter.cpp
@@ -2,5 +2,5 @@
 #include "<%- project.package_cpp -%>OnLoad.hpp"
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return margelo::nitro::<%- project.package -%>::initialize(vm);
+  return margelo::nitro::<%- project.package_cpp -%>::initialize(vm);
 }
