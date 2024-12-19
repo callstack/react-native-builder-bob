2ecb39c307ac5f67d66d9efa0fc1adc4b85cf525
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-12-19 16:30:35 +0100

    fix: write errors to stderr

--- packages/react-native-builder-bob/src/utils/logger.ts
+++ packages/react-native-builder-bob/src/utils/logger.ts
@@ -1,23 +1,28 @@
 import kleur from 'kleur';
 
 const logger =
-  (type: string, color: kleur.Color) =>
+  (
+    type: string,
+    color: kleur.Color,
+    stream: NodeJS.WriteStream = process.stdout
+  ) =>
   (...messages: unknown[]) => {
-    console.log(
-      color(kleur.bold(type)),
-      ...messages.map((message) => {
+    const message = `${color(kleur.bold(type))} ${messages
+      .map((message) => {
         if (typeof message === 'string') {
           return message.split('\n').join(`\n  `);
         } else {
           return message;
         }
       })
-    );
+      .join(' ')}`;
+
+    stream.write(message + '\n');
   };
 
 export const info = logger('ℹ', kleur.blue);
 export const warn = logger('⚠', kleur.yellow);
-export const error = logger('✖', kleur.red);
+export const error = logger('✖', kleur.red, process.stderr);
 export const success = logger('✔', kleur.green);
 
 export const exit = (...messages: unknown[]) => {
