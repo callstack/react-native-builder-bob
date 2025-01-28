f0ce21f4ef64f91c2b0ac9bced472dd0afe670fc
Burak Güner <burak.guner@callstack.com>
2025-01-28 11:07:21 +0300

    fix: legacy arch example app template fails on js

--- packages/create-react-native-library/templates/example-module-legacy/example/src/App.tsx
+++ packages/create-react-native-library/templates/example-module-legacy/example/src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { multiply } from '<%- project.slug -%>';
 
+<% if (project.native) { -%>
 export default function App() {
   const [result, setResult] = useState<number | undefined>();
 
@@ -9,6 +10,11 @@ export default function App() {
     multiply(3, 7).then(setResult);
   }, []);
 
+<% } else { -%>
+const result = multiply(3, 7);
+
+export default function App() {
+<% } -%>
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
