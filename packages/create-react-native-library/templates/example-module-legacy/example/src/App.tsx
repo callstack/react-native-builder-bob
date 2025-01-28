d17ce35fab6361f6ef8a6540929f75badb39af92
Burak Güner <burak.guner@callstack.com>
2025-01-28 11:44:06 +0300

    fix: js-only libraries have codegen config (#758)
    ### Summary
    
    Fixes #754
    
    JS-only libraries were setting the new architecture flag on. While this
    is not a preferable mental modal, we had some code associated with
    codegen that checked this flag. With this PR, JS-only libraries will set
    this flag to false. This is not an optimal solution but we will fully
    drop this flag with the deprecation of the legacy arch in the upcoming
    months.
    
    ### Test plan
    
    1. Generate a JavaScript-only library
    1. Make sure the `codegen` target isn't added to bob targets in
    `package.json`
    1. Make sure there is no `codegenConfig` in `package.json`
    1. Make sure `yarn prepare` works.

--- packages/create-react-native-library/templates/example-module-legacy/example/src/App.tsx
+++ packages/create-react-native-library/templates/example-module-legacy/example/src/App.tsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
 import { multiply } from '<%- project.slug -%>';
+import { Text, View, StyleSheet } from 'react-native';
+<% if (project.native) { -%>
+import { useState, useEffect } from 'react';
 
 export default function App() {
   const [result, setResult] = useState<number | undefined>();
@@ -9,6 +10,12 @@ export default function App() {
     multiply(3, 7).then(setResult);
   }, []);
 
+<% } else { -%>
+
+const result = multiply(3, 7);
+
+export default function App() {
+<% } -%>
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
