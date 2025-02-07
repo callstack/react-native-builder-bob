3411f2cce582a3bc621d1c7a82f854fcb51e1406
Burak Güner <burak.guner@callstack.com>
2025-02-07 11:46:04 +0300

    don't strip the nitro module code when proguard is enabled

--- packages/create-react-native-library/templates/nitro-module/android/src/main/java/com/margelo/nitro/{%- project.package_dir %}/{%- project.name %}.kt
+++ packages/create-react-native-library/templates/nitro-module/android/src/main/java/com/margelo/nitro/{%- project.package_dir %}/{%- project.name %}.kt
@@ -1,5 +1,6 @@
 package com.margelo.nitro.<%- project.package %>
 
+@DoNotStrip
 class CrnlNitro : Hybrid<%- project.name %>Spec() {
   override fun multiply(a: Double, b: Double): Double {
     return a * b
