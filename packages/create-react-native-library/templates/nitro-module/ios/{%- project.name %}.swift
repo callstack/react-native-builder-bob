fc35623e2826c566427fd984e1bed16a4f2316e9
Burak Güner <burak.guner@callstack.com>
2025-02-07 11:43:17 +0300

    remove the memory size from hybrid objects

--- packages/create-react-native-library/templates/nitro-module/ios/{%- project.name %}.swift
+++ packages/create-react-native-library/templates/nitro-module/ios/{%- project.name %}.swift
@@ -1,8 +1,4 @@
 class <%- project.name -%>: Hybrid<%- project.name -%>Spec {
-    public override var memorySize: Int {
-        getSizeOf(self)
-    }
-
     public func multiply(a: Double, b: Double) throws -> Double {
         return a * b
     }
