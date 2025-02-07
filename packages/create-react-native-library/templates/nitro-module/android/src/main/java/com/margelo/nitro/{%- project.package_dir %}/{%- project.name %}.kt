fb286f0257f21bb91674fb6de58b7675dd1ac6ed
birgernass <birgernass@users.noreply.github.com>
2025-02-07 19:48:53 +0200

    fix(crnl): fix nitro modules android template

--- packages/create-react-native-library/templates/nitro-module/android/src/main/java/com/margelo/nitro/{%- project.package_dir %}/{%- project.name %}.kt
+++ packages/create-react-native-library/templates/nitro-module/android/src/main/java/com/margelo/nitro/{%- project.package_dir %}/{%- project.name %}.kt
@@ -1,7 +1,7 @@
 package com.margelo.nitro.<%- project.package %>
 
 @DoNotStrip
-class CrnlNitro : Hybrid<%- project.name %>Spec() {
+class <%- project.name %> : Hybrid<%- project.name %>Spec() {
   override fun multiply(a: Double, b: Double): Double {
     return a * b
   }
