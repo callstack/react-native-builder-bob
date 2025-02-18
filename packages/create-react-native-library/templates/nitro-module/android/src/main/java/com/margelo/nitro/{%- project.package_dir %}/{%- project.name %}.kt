6a745660f7ba24d0c04122a46a34e63bc060abd9
Marc Rousavy <me@mrousavy.com>
2025-02-18 21:21:43 +0100

    fix: Import `@DoNotStrip`

--- packages/create-react-native-library/templates/nitro-module/android/src/main/java/com/margelo/nitro/{%- project.package_dir %}/{%- project.name %}.kt
+++ packages/create-react-native-library/templates/nitro-module/android/src/main/java/com/margelo/nitro/{%- project.package_dir %}/{%- project.name %}.kt
@@ -1,4 +1,6 @@
 package com.margelo.nitro.<%- project.package %>
+  
+import com.facebook.proguard.annotations.DoNotStrip
 
 @DoNotStrip
 class <%- project.name %> : Hybrid<%- project.name %>Spec() {
