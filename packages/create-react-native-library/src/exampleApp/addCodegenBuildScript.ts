e1a6c63591fcc90c5bbb54ec3f19646bd8f62128
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-03-15 14:21:54 +0100

    refactor: remove unnecessary checks for new arch
    Since we no longer have backward compatible template, these checks are no longer necessary

--- packages/create-react-native-library/src/exampleApp/addCodegenBuildScript.ts
+++ packages/create-react-native-library/src/exampleApp/addCodegenBuildScript.ts
@@ -3,28 +3,25 @@ import fs from 'fs-extra';
 
 // This is added to the example app's build.gradle file to invoke codegen before every build
 const GRADLE_INVOKE_CODEGEN_TASK = `
-def isNewArchitectureEnabled() {
-  return rootProject.hasProperty("newArchEnabled") && rootProject.getProperty("newArchEnabled") == "true"
+// Run Codegen during development for the example app.
+tasks.register('invokeLibraryCodegen', Exec) {
+  workingDir "$rootDir/../../"
+  def isWindows = System.getProperty('os.name').toLowerCase().contains('windows')
+
+  if (isWindows) {
+    commandLine 'cmd', '/c', 'npx bob build --target codegen'
+  } else {
+    commandLine 'sh', '-c', 'npx bob build --target codegen'
+  }
 }
 
-if (isNewArchitectureEnabled()) {
-    // Since our library doesn't invoke codegen automatically we need to do it here.
-    tasks.register('invokeLibraryCodegen', Exec) {
-      workingDir "$rootDir/../../"
-      def isWindows = System.getProperty('os.name').toLowerCase().contains('windows')
-
-      if (isWindows) {
-        commandLine 'cmd', '/c', 'npx bob build --target codegen'
-      } else {
-        commandLine 'sh', '-c', 'npx bob build --target codegen'
-      }
-    }
-    preBuild.dependsOn invokeLibraryCodegen
-}`;
+preBuild.dependsOn invokeLibraryCodegen
+`;
 
 // You need to have the files before calling pod install otherwise they won't be registered in your pod.
 // So we add a pre_install hook to the podfile that invokes codegen
 const PODSPEC_INVOKE_CODEGEN_SCRIPT = `
+  # Run Codegen during development for the example app.
   pre_install do |installer|
     system("cd ../../ && npx bob build --target codegen")
   end
