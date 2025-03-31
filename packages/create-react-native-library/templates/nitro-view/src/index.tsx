1ce6b8c4b0d6b614f0b315b6287d5f3786c9affa
Juozas Petkelis <juozas.petkelis@callstack.com>
2025-03-31 17:28:23 +0300

    fix: prettier formatting for CI

--- packages/create-react-native-library/templates/nitro-view/src/index.tsx
+++ packages/create-react-native-library/templates/nitro-view/src/index.tsx
@@ -1,8 +1,11 @@
 import { getHostComponent } from 'react-native-nitro-modules';
 import <%- project.name %>Config from '../nitrogen/generated/shared/json/<%- project.name %>Config.json';
-import type { <%- project.name %>Methods, <%- project.name %>Props } from './<%- project.name %>.nitro';
+import type {
+  <%- project.name %>Methods,
+  <%- project.name %>Props,
+} from './<%- project.name %>.nitro';
 
-export const <%- project.name %>View = getHostComponent<<%- project.name %>Props, <%- project.name %>Methods>(
-  '<%- project.name %>',
-  () => <%- project.name %>Config
-);
+export const <%- project.name %>View = getHostComponent<
+  <%- project.name %>Props,
+  <%- project.name %>Methods
+>('<%- project.name %>', () => <%- project.name %>Config);
