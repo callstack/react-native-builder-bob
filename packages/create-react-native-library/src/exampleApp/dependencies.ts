3d6a261af187873a5805a945b919758140b204f4
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-04-11 21:53:46 +0200

    fix: don't ship generated code with the library

--- packages/create-react-native-library/src/exampleApp/dependencies.ts
+++ packages/create-react-native-library/src/exampleApp/dependencies.ts
@@ -1,6 +1,5 @@
 import path from 'path';
 import fs from 'fs-extra';
-import type { TemplateConfiguration } from '../template';
 import sortObjectKeys from '../utils/sortObjectKeys';
 
 type PackageJson = {
@@ -9,24 +8,13 @@ type PackageJson = {
 
 export async function alignDependencyVersionsWithExampleApp(
   pkg: PackageJson,
-  folder: string,
-  config: TemplateConfiguration
+  folder: string
 ) {
   const examplePackageJson = await fs.readJSON(
     path.join(folder, 'example', 'package.json')
   );
 
   const PACKAGES_TO_COPY = ['react', 'react-native', '@types/react'];
-
-  if (
-    config.example === 'vanilla' &&
-    (config.project.moduleConfig === 'turbo-modules' ||
-      config.project.viewConfig === 'fabric-view')
-  ) {
-    // React Native doesn't provide the community CLI as a dependency.
-    // We have to read the version from the example app and put to the root package json
-    PACKAGES_TO_COPY.push('@react-native-community/cli');
-  }
 
   const devDependencies: Record<string, string> = {};
 
