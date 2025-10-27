6dd0eb83814893ac9e3e6ce824682a6abc902dbd
Marc Rousavy <me@mrousavy.com>
2025-10-27 13:02:51 +0100

    fix: Migrate from `TurboReactPackage` to `BaseReactPackage` (#903)
    TurboReactPackage is deprecated by RN
    
    <!-- Please provide enough information so that others can review your
    pull request. -->
    <!-- Keep pull requests small and focused on a single change. -->
    
    ### Summary
    
    See https://github.com/mrousavy/nitro/pull/994
    
    
    <!-- What existing problem does the pull request solve? Can you solve
    the issue with a different approach? -->
    
    ### Test plan
    
    <!-- List the steps with which we can test this change. Provide
    screenshots if this changes anything visual. -->

--- packages/create-react-native-library/templates/nitro-module/android/src/main/java/com/margelo/nitro/{%- project.package_dir %}/{%- project.name %}Package.kt
+++ packages/create-react-native-library/templates/nitro-module/android/src/main/java/com/margelo/nitro/{%- project.package_dir %}/{%- project.name %}Package.kt
@@ -1,11 +1,11 @@
 package com.margelo.nitro.<%- project.package %>
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfoProvider
 
-class <%- project.name -%>Package : TurboReactPackage() {
+class <%- project.name -%>Package : BaseReactPackage() {
     override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
         return null
     }
