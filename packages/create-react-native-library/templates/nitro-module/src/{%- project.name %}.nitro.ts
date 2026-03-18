bc8b9d01568160bc2c56b133e8915c26032cabe4
Satyajit Sahoo <satyajit.happy@gmail.com>
2026-03-18 09:01:39 +0100

    chore: upgrade template dependencies

--- packages/create-react-native-library/templates/nitro-module/src/{%- project.name %}.nitro.ts
+++ packages/create-react-native-library/templates/nitro-module/src/{%- project.name %}.nitro.ts
@@ -2,7 +2,7 @@ import type { HybridObject } from 'react-native-nitro-modules';
 
 export interface <%- project.name %> extends HybridObject<{
   ios: 'swift';
-  android: 'kotlin'
+  android: 'kotlin';
 }> {
   multiply(a: number, b: number): number;
 }
