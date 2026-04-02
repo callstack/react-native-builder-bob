336e0dbd05c2993a4ea97ef92d78af896cdd09c6
riteshshukla04 <riteshshukla2381@gmail.com>
2026-04-02 22:31:20 +0530

    chore: nitro-modules-upgrade

--- packages/create-react-native-library/templates/nitro-common/android/src/main/cpp/cpp-adapter.cpp
+++ packages/create-react-native-library/templates/nitro-common/android/src/main/cpp/cpp-adapter.cpp
@@ -1,6 +1,11 @@
 #include <jni.h>
 #include "<%- project.package_cpp -%>OnLoad.hpp"
 
+#include <fbjni/fbjni.h>
+
+
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return margelo::nitro::<%- project.package_cpp -%>::initialize(vm);
+  return facebook::jni::initialize(vm, []() {
+    margelo::nitro::<%- project.package_cpp -%>::registerAllNatives();
+  });
 }
